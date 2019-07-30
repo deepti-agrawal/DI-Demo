0ded1513763ac4f58b1b8305f6eaf7748cf03f81
Deepti <Deepti.sms@gmail.com>
2019-07-29 21:47:08 -0500

    learned and added @primary and @qualifier

--- src/main/java/com/example/didemo/DiDemoApplication.java
+++ src/main/java/com/example/didemo/DiDemoApplication.java
@@ -14,6 +14,8 @@ public class DiDemoApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DiDemoApplication.class, args);
 
+        MyController controller = (MyController) ctx.getBean("myController");
+        System.out.println(controller.sayHello());
         System.out.println(ctx.getBean(MyController.class).sayHello());
         System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
         System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
